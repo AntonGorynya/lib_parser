d66d96de7be6d0f636b76d52a741b868217a1e82
SD <antongorynya1@gmail.com>
2023-04-25 22:48:45 +0300

    add readme and req

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ SITE = 'https://tululu.org/'
 SOURCE_TEXT = 'https://tululu.org/txt.php?id={id}'
 BOOK_PAGE = 'https://tululu.org/b{id}/'
 
+
 def download_txt(url, filename, path='books'):
     filename = os.path.join(path, filename)
     os.makedirs(path, exist_ok=True)
@@ -18,7 +19,7 @@ def download_txt(url, filename, path='books'):
     with open(filename, 'wb') as file:
         file.write(response.content)
 
-def download_image (url, path='books'):
+def download_image(url, path='books'):
     _, filename = os.path.split(url)
     os.makedirs(path, exist_ok=True)
     response = requests.get(url, allow_redirects=False)
@@ -26,12 +27,15 @@ def download_image (url, path='books'):
     with open(filename, 'wb') as file:
         file.write(response.content)
 
+
 def check_for_redirect(response):
     if 'text/plain' not in response.headers['Content-Type']:
         raise requests.HTTPError
 
+
 def serialize_name(book_name):
-    return re.sub(r'[^\w\. ]+','', book_name)
+    return re.sub(r'[^\w\. ]+', '', book_name)
+
 
 def parse_book_page(soup):
     book_title, author = [
@@ -45,12 +49,13 @@ def parse_book_page(soup):
     book_genre = soup.find('span', {'class': 'd_book'}).find('a').text
     return {
         'author': author,
-        'book_title': book_title,
-        'book_img': book_img,
+        'title': book_title,
+        'img': book_img,
         'book_description': book_description,
         'book_comments': book_comments,
         'book_genre': book_genre,
     }
+
 
 def create_parser():
     parser = argparse.ArgumentParser(
@@ -61,7 +66,6 @@ def create_parser():
     return parser
 
 
-
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
@@ -70,15 +74,13 @@ if __name__ == '__main__':
         text_url = SOURCE_TEXT.format(id=i)
         book_url = BOOK_PAGE.format(id=i)
 
-
         response = requests.get(book_url, allow_redirects=False)
         response.raise_for_status()
         if not response.is_redirect:
             soup = BeautifulSoup(response.text, 'lxml')
-            parse_book_page(soup)
-
-        # try:
-        #     download_txt(text_url, book_title)
-        #     download_image(book_img)
-        # except requests.HTTPError:
-        #     pass
+            serialize_book = parse_book_page(soup)
+            try:
+                download_txt(text_url, serialize_book['title'])
+                download_image(serialize_book['img'])
+            except requests.HTTPError:
+                pass
